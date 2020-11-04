22e3188b4aab2adfe76b157835e138f7b7e013b4
Jonathan Lifflander <jliffla@sandia.gov>
2020-11-04 02:33:52 +0000

    Update docs (auto-build)

--- docs/html/search/related_f.js
+++ docs/html/search/related_f.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['runinepochcollective',['runInEpochCollective',['../structvt_1_1sched_1_1_scheduler.html#a1724d1352c428bcb2bfc5a32a6a27404',1,'vt::sched::Scheduler']]],
-  ['runinepochrooted',['runInEpochRooted',['../structvt_1_1sched_1_1_scheduler.html#a915d5c24603b0f28e812e95106d2ca09',1,'vt::sched::Scheduler']]],
+  ['runinepochcollective',['runInEpochCollective',['../structvt_1_1sched_1_1_scheduler.html#a71bf858d697b14435f4b7e525d2dffde',1,'vt::sched::Scheduler']]],
+  ['runinepochrooted',['runInEpochRooted',['../structvt_1_1sched_1_1_scheduler.html#a792343e9a39ba8661b35c3c9e55f3541',1,'vt::sched::Scheduler']]],
   ['virtualcontextattorney',['VirtualContextAttorney',['../structvt_1_1vrt_1_1_virtual_context.html#a3c098ac4f050b85316f4ae9eef22a43e',1,'vt::vrt::VirtualContext']]],
   ['virtualcontextmanager',['VirtualContextManager',['../structvt_1_1vrt_1_1_virtual_context_attorney.html#a93a95e040b1875a105c62f3465731412',1,'vt::vrt::VirtualContextAttorney']]],
   ['virtualmakeclosure',['VirtualMakeClosure',['../structvt_1_1vrt_1_1_virtual_context_attorney.html#a6d27b32cd35d056a4d8bf6ba43f28f62',1,'vt::vrt::VirtualContextAttorney::VirtualMakeClosure()'],['../structvt_1_1vrt_1_1_virtual_info.html#a6d27b32cd35d056a4d8bf6ba43f28f62',1,'vt::vrt::VirtualInfo::VirtualMakeClosure()']]]
